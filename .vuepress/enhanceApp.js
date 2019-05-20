60d260d4807fcb01f07307439d2abb0c44bc0ee7
johnniang <johnniang@riseup.net>
2019-05-20 17:55:54 +0800

    Fix build error

--- .vuepress/enhanceApp.js
+++ .vuepress/enhanceApp.js
@@ -1,8 +1,13 @@
-import ElementUI from 'element-ui';
+import locale from 'element-ui/lib/locale/lang/en';
 import 'element-ui/lib/theme-chalk/index.css';
 
-export default ({
-    Vue
-}) => {
-    Vue.use(ElementUI)
+export default ({ Vue, isServer }) => {
+  /**
+   * Only import element-ui under client side.
+   */
+  if (!isServer) {
+    import('element-ui').then(elementUI => {
+      Vue.use(elementUI.default, { locale })
+    })
+  }
 }
